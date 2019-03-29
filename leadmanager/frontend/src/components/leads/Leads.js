0ef629ca40332f97524c7c772c027761b31f93e1
Matt Kong <mattkong@umich.edu>
2019-03-29 19:20:24 -0400

    Set up redux. TODO: Add actions to the reducers.

---
+++ leadmanager/frontend/src/components/leads/Leads.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class Leads extends React.Component {
+	render() {
+		return (
+			<div>
+				<h1>Leads List</h1>
+			</div>
+		);
+	}
+}
---
+++ leadmanager/frontend/src/reducers/leads.js
@@ -0,0 +1,15 @@
+import { GET_LEADS } from '../actions/types.js';
+
+const initialState = {
+	leads: []
+}
+
+export default function(state = initialState, action) {
+	switch(action.type) {
+		case GET_LEADS:
+			return {
+				...state,
+				leads: action.payload
+			}
+	}
+}
