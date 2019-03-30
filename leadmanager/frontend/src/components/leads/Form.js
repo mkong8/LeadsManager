db79692e730dbc558acb168d94ee9eafedef6914
Matt Kong <mattkong@umich.edu>
2019-03-30 19:36:48 -0400

    Implement basic GET, DELETE, POST actions with redux.

--- leadmanager/frontend/src/components/leads/Form.js
+++ leadmanager/frontend/src/components/leads/Form.js
@@ -1,11 +1,79 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { createLead } from "../../actions/leads";
 
-export default class Form extends React.Component {
+export class Form extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			name: '',
+			email: '',
+			message: ''
+		}
+
+		this.onChange = this.onChange.bind(this);
+		this.onSubmit = this.onSubmit.bind(this);
+	}
+
+	static propTypes = {
+		createLead: PropTypes.func.isRequired
+	};
+
+	onChange = e => this.setState({ [e.target.name ]: e.target.value });
+
+	onSubmit = e => {
+		e.preventDefault();
+		const { name, email, message } = this.state;
+		const lead = { name, email, message };
+		this.props.createLead(lead);
+	};
+
 	render() {
+		const { name, email, message } = this.state;
 		return (
-			<div>
-				<h1>Add Lead Form</h1>
+			<div className="card card-body mt-4 mb-4">
+				<h1>Add Lead</h1>
+				<form onSubmit={this.onSubmit}>
+					<div className="form-group">
+						<label>Name</label>
+						<input
+							className="form-control"
+							type="text"
+							name="name"
+							onChange={this.onChange}
+							value={name}
+						/>
+					</div>
+					<div className="form-group">
+						<label>Email</label>
+						<input
+							className="form-control"
+							type="email"
+							name="email"
+							onChange={this.onChange}
+							value={email}
+						/>
+					</div>
+					<div className="form-group">
+						<label>Message</label>
+						<input
+							className="form-control"
+							type="text"
+							name="message"
+							onChange={this.onChange}
+							value={message}
+						/>
+					</div>
+					<div className="form-group">
+						<button type="submit" className="btn btn-primary">
+							Submit
+						</button>
+					</div>
+				</form>
 			</div>
 		);
 	}
 }
+
+export default connect(null, { createLead })(Form)
--- leadmanager/frontend/src/reducers/leads.js
+++ leadmanager/frontend/src/reducers/leads.js
@@ -1,4 +1,4 @@
-import { GET_LEADS } from '../actions/types.js';
+import { GET_LEADS, DELETE_LEAD, CREATE_LEAD } from '../actions/types.js';
 
 const initialState = {
 	leads: []
@@ -10,6 +10,18 @@ export default function(state = initialState, action) {
 			return {
 				...state,
 				leads: action.payload
+			};
+		case DELETE_LEAD:
+			return {
+				...state,
+				leads: state.leads.filter(lead => lead.id !== action.payload)
 			}
+		case CREATE_LEAD:
+			return {
+				...state,
+				leads: [...state.leads, action.payload]
+			}
+		default:
+			return state;
 	}
 }
